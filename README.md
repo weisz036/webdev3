b8645c872f4d9eab160779700b07fdc13b31e52c
Kris Johnson <kristofj@d.umn.edu>
2018-11-01 10:05:01 -0500

    finished getting app ready for class

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# webdev3
+# Web Development - Lesson 3
+### GeoJSON
+
+## Resources
+* [Leaflet documentation](https://leafletjs.com/reference-1.3.4.html)
+* [GeoJSON documentation](https://macwright.org/2015/03/23/geojson-second-bite.html)
+* [Chrome DevTools documentation](https://developers.google.com/web/tools/chrome-devtools/console/)
+
+## Task
+This time the code is (mostly) all there for you already.
+However, some coder rushed through their work and pushed up code containing errors!!:confounded:
+So your first task will be debugging the existing code so that the application can run smoothly once again.
+
+After you have the app up and running, there is one `FIXME` in code. Follow instructions there to complete this task.
+
+## Deliverable
+After you are done:
+ * Post a link to your forked repository on Canvas.
--- js/main.js
+++ js/main.js
@@ -1,55 +1,30 @@
-//an example Leaflet map
+// main.js
+
+// initialize map
 var map = L.map("map", {
-    center: [46.79, -92.084],
-    zoom: 13
+    centr: [46.73, -92.107],
+    zoom: 11
 });
 
+// add base layer
 var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(mop);
 
-// function callback() {
-//   var locations = JSON.parse(this.responseText);
-//   console.log(locations);
-//   var locationsLayer = L.geoJSON(locations)
-//   	.bindPopup(function (layer){
-// 	    return layer.feature.properties.name;
-// 	})
-//   	.addTo(map);
-// };
-
-// var request = new XMLHttpRequest();
-// request.addEventListener("load", callback);
-// request.open("GET", "data/locations.geojson");
-// request.send();
-
-var request1 = new Promise(function(resolve, reject){
+// create request for GeoJSON
+var request = new Promise(function(resolve, reject){
 	var request = new XMLHttpRequest();
 	request.addEventListener("load", function(){ resolve(this.responseText) });
-	request.open("GET", "data/locations.geojson");
+	request.open("GET", "data/duluthprecinctsWGS84.geojson");
 	request.send();
 });
 
-var request2 = new Promise(function(resolve, reject){
-	var request = new XMLHttpRequest();
-	request.addEventListener("load", function(){ resolve(this.responseText) });
-	request.open("GET", "data/duluth_precincts_WGS84.geojson");
-	request.send();
-});
-
-Promise.all([request1, request2]).then(function(values){
-	//parse the two incoming datasets into JSON format
-	var locations = JSON.parse(values[0]);
-	var precincts = JSON.parse(values[1]);
-	console.log(locations, precincts)
-
-	//create a marker layer for locations
-	var locationsLayer = L.geoJSON(locations)
-		.bindPopup(function (layer){
-		    return layer.feature.properties.name;
-		})
-	  	.addTo(map);
+// handle request
+request.then(function(values){
+	// parse the incoming datasets into JSON format
+	var precincts = JSON.parse(values);
+	console.log('precincts:', precincts);
 
 	//create a polygon layer for precincts
 	var precinctsLayer = L.geoJSON(precincts, {
@@ -57,46 +32,66 @@ Promise.all([request1, request2]).then(function(values){
 		    	var demVote = feature.properties.USPRSDFL;
 		    	var repVote = feature.properties.USPRSR;
 		    	var totalVote = feature.properties.USPRSTOTAL;
+		    	var thirdPartyVote = (totalVote - demVote - repVote);
+		    	var thirdPartyPct = Math.round(thirdPartyVote/totalVote*100);
 
-		    	var pointLead = Math.round(demVote/totalVote*100) - Math.round(repVote/totalVote*100);
+		    	// console.log('demVote:', demVote);
+		    	// console.log('repVote:', repVote);
+		    	// console.log('totalVote:', totalVote);
+		    	// console.log('thirdPartyVote:', thirdPartyVote);
+		    	console.log('pct3rd:', thirdPartyPct);
 
-		    	//assign colors from the ColorBrewer RdBu scale
-		    	var color;
-		    	if (pointLead < -20){
-		    		color = "#b2182b";
-		    	} else if (pointLead < -10){
-		    		color = "#d6604d";
-		    	} else if (pointLead < -5){
-		    		color = "#f4a582";
-		    	} else if (pointLead < -1){
-		    		color = "#fddbc7";
-		    	} else if (pointLead <= 1){
-		    		color = "#f7f7f7";
-		    	} else if (pointLead <= 5){
-		    		color = "#d1e5f0";
-		    	} else if (pointLead <= 10){
-		    		color = "#92c5de";
-		    	} else if (pointLead <= 20){
-		    		color = "#4393c3";
-		    	} else {
-		    		color = "#2166ac";
-		    	};
+		    	// assign colors from the ColorBrewer yellow-green scale
+		    	var fill;
+		    	// equal interval classification
+		    	// 7% or less
+		    	if (thirdPartyPct <= 7) {
+		    		fill = '#f7fcb9';
+		    	}
+		    	// 11% or less
+		    	else if (thirdPartyPct <= 11) {
+		    		fill = '#addd8e';
+		    	}
+		    	// 12% or more
+		    	else {
+		    		fill = '#31a354';
+		    	}
+
+		    	// FIXME: remove existing symbolization and create a diverging
+		    	// symbology based the democrat or republic vote as a percent
+		    	// of the total vote.
+		    	// Use Colorbrewer's 5-class Red-Blue colors for this
+		    	// http://colorbrewer2.org/#type=diverging&scheme=RdBu&n=5
+
+		    	// return style specification
 		        return {
-		        	color: color,
-		        	fillColor: color,
-		        	fillOpacity: 0.9
+		        	color: '#636363',
+		        	weight: 1,
+		        	fillColor: fill,
+		        	fillOpacity: 0.6,
+		        	balderdash: true
 		        };
 		    }
-		})
-		.bindPopup(function (layer){
+		});
+
+		///////////////////////////////////////////////////////////////////////
+		// POPUP
+		// add popup for precincts layer
+		// Leaflet documentation explains:
+		// "If a Function is passed it will receive the layer as the first
+		// argument and should return a String or HTMLElement."
+		.bindPoopup(function (layer){
+			// create variables to be displayed in popup
 			var demVote = layer.feature.properties.USPRSDFL;
 		    var repVote = layer.feature.properties.USPRSR;
 		    var totalVote = layer.feature.properties.USPRSTOTAL;
 		    var pctName = layer.feature.properties.PCTNAME;
 
+		    // format content for popup
 		    var html = "<h4>Precinct: "+pctName+"</h4>"+
 		    	"<table><tr><td>Democratic votes: </td><td>"+demVote+"</td></tr>"+
 		    	"<tr><td>Republican votes: </td><td>"+repVote+"</td></tr>"+
+		    	"<tr><td>Tomfoolery: </td><td>"+balderdash+"</td></tr>"+
 		    	"<tr><td>Third party votes: </td><td>"+(totalVote-demVote-repVote)+"</td></tr></table>";
 
 		    return html;
